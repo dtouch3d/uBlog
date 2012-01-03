64285b009e9c3f65b3f8b578cb2381435cdfd433
Chris <chrspapadop@gmail.com>
2012-01-04 00:08:45 +0200

    update fixed

--- controllers/update_profile.php
+++ controllers/update_profile.php
@@ -2,14 +2,14 @@
 		if(!isset($_POST['name'])) {
 			require_once('models/user.php');
 			global $user;
-			$userid=9;
+			$userid=$_SESSION['userid'];
 			$user = getProfileDetails($userid);
 			require "views/header.php";
 			require "views/update_profile/view.php";
 			require "views/footer.php";
 		}
 		else {
-			$userid=9;		//demo
+			$userid=$_SESSION['userid'];		
 			updateProfile($userid, $_POST['name'], $_POST['surname'], $_POST['location'], $_POST['occupation'], $_POST['interests'], $_POST['website'] );
 			header('Location: index.php?redpage=profile');
 		}
