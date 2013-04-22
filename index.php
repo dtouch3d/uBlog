44a3627fd2dd9a164eb5a9e3acf52a9e16d192f9
Tatsis Dimitrios <dtouch3d@gmail.com>
2013-04-22 20:27:57 +0300

    Cleanup index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 	global $settings;
-    $settings = require( 'settings.php' );
+    	$settings = require( 'settings.php' );
 	require "models/db.php";
-    require "models/user.php";
+    	require "models/user.php";
 	require "models/blog.php";
-    //$user = getCurrentUser();
-    if (!isset($_SESSION['userid'])) {
+
+    	if (!isset($_SESSION['userid'])) {
 		$redpage = 'login';
 	}
 	else {
@@ -14,22 +14,12 @@
 	
 	if( isset( $_GET[ 'redpage' ] )&&isset($_SESSION['userid']) ){
 		$redpage = $_GET[ 'redpage' ];   
-    }
+    	}
 	if (isset($_GET['redpage'])) {
 		if ($_GET['redpage']=='register'&&!isset($_SESSION['userid'])) {
 			$redpage='register';
 		}
 	}
-    /*
-    if( $user == false && !in_array( $page, array( 'login', 'register', 'dologin', 'doregister' ) ) ){
-        header( 'Location: ./?page=login' );
-        exit();
-    }
-    $whitelist = array( 'posts', 'post', 'dopost', 'docomment', 'login', 'register', 'dologin', 'doregister' );
-    if( !in_array( $page, $whitelist ) ){
-        die( "Could not find page $page." );
-    }*/
- 
 	require "controllers/{$redpage}.php";
 
 ?>
